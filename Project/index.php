013f310762184e4f1070c20671ce81c953c0f011
Darko <darkohrnjak06@gmail.com>
2025-11-26 20:04:20 +0100

    dodano zapisivanje kategorija u json

--- Project/index.php
+++ Project/index.php
@@ -10,6 +10,7 @@ require_once "Factories/ProductFactory.php";
 require_once "autoload.php";
 
 use Project\Factories\ProductFactory;
+use Project\Factories\CategoryFactory;
 use Project\Services\JsonDB;
 
 $product = ProductFactory::create(
@@ -33,6 +34,16 @@ $p2 = ProductFactory::create(
 );
 
 $db->save("products.json",$p2->toArray());
+echo "<hr>";
+
+$cat1 = CategoryFactory::create("Povrće","Ovo je povrce");
+$p3 = ProductFactory::create(
+    "krumpir",
+    3.99,
+    $cat1->name,
+    $cat1->description
+);
+$db->save("products.json",$p3->toArray());
 echo "<hr>";
 
 ?>
